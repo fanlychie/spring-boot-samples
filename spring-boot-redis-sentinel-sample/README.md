8d7cd26eb090eb7bd028a2f766c498e76d49cb1e
fanlychie <fanlychie@yeah.net>
2018-07-11 18:00:48 +0800

    Spring Boot Redis Sentinel 示例项目

---
+++ spring-boot-redis-sentinel-sample/README.md
@@ -0,0 +1,3 @@
+# 简述
+
+Spring Boot Redis Sentinel 示例项目
---
+++ spring-boot-redis-sentinel-sample/src/main/java/org/fanlychie/config/ApplicationConfig.java
@@ -0,0 +1,30 @@
+package org.fanlychie.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        // 字符串序列化
+        RedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // 对象转为JSON串序列化
+        RedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer(new ObjectMapper());
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(jsonRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setHashValueSerializer(jsonRedisSerializer);
+        return template;
+    }
+
+}
